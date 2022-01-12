b81353aa5b274df101d490d72e685cc687ad2948
Nathalie Faber <nathalie_faber@hotmail.com>
2022-01-12 12:59:57 +0100

    added pages and styles

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   get '/about', to: "pages#about"
   get '/contact', to: "pages#contact"
   get '/booking', to: "pages#booking"
+  get '/music', to: "pages#music"
+  get '/photography', to: "pages#photography"
 
 end
